6cf6f64ab1bec77fde6bf6fcd26f163727315a40
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-19 15:40:09 +0530

    code modify

---
+++ src/main/java/com/infy/ci/batdbamqpservice/BatdbamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.batdbamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatdbamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatdbamqpserviceApplication.class, args);
+	}
+
+}
