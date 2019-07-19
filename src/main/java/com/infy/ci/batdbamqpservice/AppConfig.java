20e612e563e6ff904a0025a23d40c9d323ef43e7
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-19 16:55:33 +0530

    code modify

---
+++ src/main/java/com/infy/ci/batdbamqpservice/AppConfig.java
@@ -0,0 +1,10 @@
+package com.infy.ci.batdbamqpservice;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "com.infy.ci.batdbamqpservice" })
+public class AppConfig {
+
+}
